a8357e973cd54d2299f9d9cdf0765c556792480e
Renato Italo <renato.filizzola@gmail.com>
2015-06-18 23:00:07 -0300

    Criei um metodo para ler do omdb api e um metodo para inserir no parse os dados recebidos (JSON)

--- main.py
+++ main.py
@@ -4,6 +4,7 @@ import sys
 import random
 import urllib
 import json
+import httplib
 
 def parser_csv():
 	
@@ -20,14 +21,28 @@ def parser_csv():
 def main():
 	entrada = parser_csv()
 	#print entrada
-	readOmdbApi(entrada)
+	data = readOmdbApi(entrada)
 
 def readOmdbApi(entrada):
+	dataList = []
 	for i in range(len(entrada)):
 		url = "http://www.omdbapi.com/?i=tt" + entrada[i]
 		response = urllib.urlopen(url);
 		data = json.loads(response.read())
-		print data
+		importDataToParse(data)
+		print i
+	return dataList
+
+def importDataToParse(data):
+	connection = httplib.HTTPSConnection('api.parse.com', 443)
+	connection.connect()
+	connection.request('POST', '/1/classes/Movies', json.dumps(data), {
+       	"X-Parse-Application-Id": "wJm8XMGGzNX4EPhneuLUa6sC9gujI5Jiwnfnxl8k",
+       	"X-Parse-REST-API-Key": "2nc2p0ePr7RxrIVFOon8ofFZqUFfdSjegXG8qNTB",
+       	"Content-Type": "application/json"
+    })
+	results = json.loads(connection.getresponse().read())
+	print results
 
 if __name__ == '__main__':
 	main()
